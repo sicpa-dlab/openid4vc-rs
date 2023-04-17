2abdf93555ae261815de4d93e2a2d3df63b4b2fc
Berend Sliedrecht <61358536+blu3beri@users.noreply.github.com>
2023-04-17 20:21:43 +0200

    feat: example in docker (#19)

--- crates/openid4vci-grpc/src/server.rs
+++ crates/openid4vci-grpc/src/server.rs
@@ -7,7 +7,7 @@ use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+    let addr = "0.0.0.0:50051".parse()?;
     let issuer = GrpcCredentialIssuer::default();
     let access_token = GrpcAccessToken::default();
 
